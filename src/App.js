c6e2b30b92d145c0f584a5d13dba0f53c731ba71
Caio Carvalho <caioalmeidaacl@gmail.com>
2024-07-08 10:48:27 -0300

    feat: v1 finish

--- src/App.js
+++ src/App.js
@@ -12,63 +12,71 @@ import { useState } from 'react';
 function App() {
   const [shareOpen, setShareOpen] = useState(false);
   return (
-    <main className='p-4'>
-      <div className='flex flex-col bg-white w-full desktop:max-w-6xl rounded-2xl'>
-        <section class="zoom-container rounded-t-2xl">
-          <img src={Image} alt='' className='translate-y-5  scale-125'/>        
+    <main className="mobile:p-4 ">
+      <div className="flex flex-col desktop:flex-row bg-white w-full desktop:max-w-5xl rounded-2xl">
+        <section className="zoom-container desktop:rounded-none mobile:rounded-t-2xl pt-2 desktop:rounded-l-2xl">
+          <img 
+            src={Image} 
+            alt="" 
+            className="mobile:translate-y-5 desktop:translate-y-0 desktop:scale-105 mobile:scale-125 object-cover" 
+          />
         </section>
 
-        <article className='p-8 pb-4'>
-          <div className='items-center desktop:max-w-lg flex flex-col '>
-            <div className='my-auto'> 
-                <h1 className="font-bold text-very-dark-grayish-blue text-base desktop:text-2xl pb-4 pr-4">Shift the overall look and feel by adding these wonderful 
-                touches to furniture in your home</h1> 
-                <p className='text-body desktop:text-body text-desaturated-dark-blue pr-6 '>Ever been in a room and felt like something was missing? Perhaps 
-                it felt slightly bare and uninviting. I’ve got some simple tips 
-                to help you make any room feel complete.</p>
+        
+        <div className="desktop:w-3/4">
+          <article className="p-8 desktop:px-10 desktop:pt-12 desktop:pb-7">
+            <div className="items-center desktop:max-w-lg flex flex-col ">
+              <div> 
+                  <h1 className="font-bold text-very-dark-grayish-blue mobile:text-base desktop:text-2xl pb-4 mobile:pr-4 desktop:pr-10 ">Shift the overall look and feel by adding these wonderful 
+                  touches to furniture in your home</h1> 
+                  <p className="text-sm desktop:text-base text-desaturated-dark-blue pr-8">Ever been in a room and felt like something was missing? Perhaps 
+                  it felt slightly bare and uninviting. I’ve got some simple tips 
+                  to help you make any room feel complete.</p>
+              </div>
             </div>
-          </div>
-        </article>
+          </article>
 
-        <section className='relative w-full py-3  px-4'>
-          <div className='flex w-full items-center px-4 justify-between'>
-                <div className='flex items-center'>
-                  <img src={ProfilePicture} alt='' className='h-10 rounded-full'/>
-                  <div className='flex flex-col pl-2 '>
-                    <h2 className='text-sm desktop:text-lg font-bold text-very-dark-grayish-blue'>Michelle Appleton</h2>
-                    <p className='text-body text-desaturated-dark-blue'>28 Jun 2020</p>  
+          <section className="relative w-full mobile:py-3 mobile:pb-4 px-4 desktop:pr-18  desktop:pl-6 ">
+            <div className="flex w-full items-center px-4 justify-between">
+
+                  <div className="flex items-center">
+                    <img src={ProfilePicture} alt="" className="h-10 desktop:h-12 rounded-full"/>
+                    <div className="flex flex-col pl-4 ">
+                      <h2 className="text-sm desktop:text-base font-bold text-very-dark-grayish-blue">Michelle Appleton</h2>
+                      <p className="text-body desktop:text-sm text-desaturated-dark-blue">28 Jun 2020</p>  
+                    </div>
+
+
+                    <div className={`${shareOpen ? "absolute z-50 right-0 desktop:-right-7 desktop:bottom-20 mobile:inset-y-0 flex desktop:justify-center h-full w-full desktop:w-3/6 desktop:py-8 rounded-b-xl desktop:rounded-none desktop:rounded-xl items-center arrow" : "hidden"}  bg-very-dark-grayish-blue`}>
+                      <ul className="flex items-center mobile:ml-7 ">
+                        <li>
+                          <p className="text-base desktop:text-lg tracking-widest uppercase  text-grayish-blue">Share</p>             
+                        </li>
+                        <li className="mx-3">
+                          <img src={IconFacbook} alt=""/>
+                        </li>
+                        <li className="mx-3">
+                          <img src={IconTwitter} alt=""/>
+                        </li>
+                        <li className="mx-3">
+                          <img src={IconPinterest} alt=""/>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
 
-
-                  <div className={`${shareOpen ? ' absolute z-50 left-0 top-0 flex h-full w-full rounded-b-xl items-center ' : 'hidden'}  bg-very-dark-grayish-blue`}>
-                    <ul className='flex items-center ml-5'>
-                      <li>
-                        <p className='text-base leading-8 tracking-widest uppercase pl-2 text-grayish-blue'>Share</p>             
-                      </li>
-                      <li className='mx-3'>
-                        <img src={IconFacbook} alt=''/>
-                      </li>
-                      <li className='mx-3'>
-                        <img src={IconTwitter} alt=''/>
-                      </li>
-                      <li className='mx-3'>
-                        <img src={IconPinterest} alt=''/>
-                      </li>
-                    </ul>
+                  
+                  <div className="z-50">
+                    <button 
+                    onClick={() => setShareOpen(!shareOpen)}
+                    className={`${shareOpen ? "bg-desaturated-dark-blue": "bg-light-grayish-blue border-light-grayish-blue border " } rounded-full p-3 `}
+                    >
+                      <img src={shareOpen ? IconShareWhite : IconShare} alt="" width={15} height={13} />
+                    </button>
                   </div>
-                </div>
-
-                
-                <div className='z-50'>
-                  <button 
-                  onClick={() => setShareOpen(!shareOpen)}
-                  className={`${shareOpen ? 'bg-desaturated-dark-blue  ': 'bg-light-grayish-blue border-light-grayish-blue border ' } rounded-full p-3`}
-                  >
-                    <img src={shareOpen ? IconShareWhite : IconShare} alt='' width={15} height={13} />
-                  </button>
-                </div>
-            </div>
-        </section>
+              </div>
+          </section>
+        </div>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,37 +3,38 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     screens:{
-      desktop: "1440px",
+      'desktop': { 'min': '1440px' },
+      'mobile': { 'min': '375px', 'max': '1439px' },
     },
     extend: {
+        padding: {
+          '18': '72px',
+        },
+        fontSize: {
+          lg: ['18px', '28px'],
+          body: "13px",
+        },
+        colors: {
+          "very-dark-grayish-blue": "hsl(217, 19%, 35%)",
+          "desaturated-dark-blue": "hsl(214, 17%, 51%)",
+          "grayish-blue": "hsl(212, 23%, 69%)",
+          "light-grayish-blue": "hsl(212, 16%, 92%)",
+        },
+        letterSpacing: {
+          tightest: '-.075em',
+          tighter: '-.05em',
+          tight: '-.025em',
+          normal: '0',
+          wide: '.025em',
+          wider: '.05em',
+          widest: '.1em',
+          widest: '.40em',
+        },
+        fontFamily: {
+          manrope: ["Manrope", "sans-serif"]
+        },
     },
-    fontSize: {
-      sm: ['14px', '20px'],
-      base: ['16px', '24px'],
-      lg: ['20px', '28px'],
-      xl: ['24px', '32px'],
-      body: "13px",
-    },
-    colors: {
-      "white": "#FFFFFF",
-      "very-dark-grayish-blue": "hsl(217, 19%, 35%)",
-      "desaturated-dark-blue": "hsl(214, 17%, 51%)",
-      "grayish-blue": "hsl(212, 23%, 69%)",
-      "light-grayish-blue": "hsl(212, 16%, 92%)",
-    },
-    fontFamily: {
-      manrope: ["Manrope", "sans-serif"]
-    },
-    letterSpacing: {
-      tightest: '-.075em',
-      tighter: '-.05em',
-      tight: '-.025em',
-      normal: '0',
-      wide: '.025em',
-      wider: '.05em',
-      widest: '.1em',
-      widest: '.40em',
-    },
+    
   },
   plugins: [],
 }
